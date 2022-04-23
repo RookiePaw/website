5e6baf9d21e068b08069c126817dd7da599cb7d3
Ushie <ahmadali012006@gmail.com>
2022-04-24 02:47:14 +0300

    Switch from TS to JS because I'm going insane (im new be nice)

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,43 @@
+const plugins_1 = require("./config/plugins");
+const config_1 = require("vuepress/config");
+module.exports = {
+    plugins: plugins_1.pluginsConfig
+};
+module.exports = (0, config_1.defineConfig4CustomTheme)(function (ctx) { return ({
+    theme: "vt",
+    title: "Home",
+    themeConfig: {
+        enableDarkMode: true,
+        repo: "https://github.com/SamsungDiscord/samsungdiscord.github.io",
+        // logo: "/logo.svg",
+        prevLinks: false,
+        nav: [
+            { text: "Guide", link: "/guide/" },
+            { text: "Forms", link: "/forms/" },
+        ],
+        sidebar: {
+            "/guide/": [
+                {
+                    title: "Introduction",
+                    collapsable: false,
+                    children: ["/guide/", "/guide/getting-started"],
+                },
+                {
+                    title: "Guide",
+                    collapsable: false,
+                    children: [
+                        "/guide/firmware",
+                        "/guide/shizuku",
+                    ],
+                },
+            ],
+            "/forms/": [
+                {
+                    title: "Introduction",
+                    collapsable: false,
+                    children: ["/forms/", "/forms/getting-started"],
+                },
+            ],
+        },
+    },
+}); });
